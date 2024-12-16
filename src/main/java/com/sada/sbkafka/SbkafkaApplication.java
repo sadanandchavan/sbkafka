2c1600860974476cccda48bbac30d602cd8b30f3
sadanand chavan <sadanand.chavan@billdesk.com>
2024-12-16 17:53:53 +0530

    Spring boot kafka producer

---
+++ src/main/java/com/sada/sbkafka/SbkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sada.sbkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbkafkaApplication.class, args);
+	}
+
+}
